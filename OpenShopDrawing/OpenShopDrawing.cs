a2ff1976b69044f20d6788681b757ead62d84751
Cory LePage <cory@lepagegroup.ca>
2017-11-22 17:18:04 -0800

    Initial commit

---
+++ OpenShopDrawing/OpenShopDrawing.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+
+namespace OpenShopDrawing
+{
+	public partial class Program
+	{
+		public static class OpenShopDrawing
+		{
+			private static DrawingHandler Handler => new DrawingHandler();
+
+			public static void Run()
+			{
+				var selector = new Tekla.Structures.Model.UI.ModelObjectSelector();
+				var modelObjects = selector.GetSelectedObjects();
+				modelObjects.MoveNext();
+
+				//Get picked part
+				var picked = modelObjects.Current as Tekla.Structures.Model.Part;
+
+				if (picked == null)
+					return;
+
+				//Check if numbering up to date
+				if (!Operation.IsNumberingUpToDate(picked))
+					return;
+
+				//Get drawings
+				var drawingCollection = Handler.GetDrawings();
+				if (drawingCollection.GetSize() < 1)
+					return;
+
+				//Get drawings by type
+				var drawings = new List<Drawing>();
+				foreach (Drawing drawing in drawingCollection)
+					drawings.Add(drawing);
+
+				var assemblyDrawings = new List<AssemblyDrawing>(drawings.OfType<AssemblyDrawing>());
+				var singlePartDrawings = new List<SinglePartDrawing>(drawings.OfType<SinglePartDrawing>());
+
+				//Get assembly mark
+				var assemblyMark = string.Empty;
+				picked.GetReportProperty("ASSEMBLY_POS", ref assemblyMark);
+				if (string.IsNullOrEmpty(assemblyMark))
+					return;
+
+				var singlePartDrawing = singlePartDrawings
+					.FirstOrDefault(s => GetDrawingUsableMark(s) == assemblyMark);
+
+				var assemblyDrawing = assemblyDrawings
+					.FirstOrDefault(s => GetDrawingUsableMark(s) == assemblyMark);
+
+				if (singlePartDrawing != null)
+					Handler.SetActiveDrawing(singlePartDrawing);
+				else if (assemblyDrawing != null)
+					Handler.SetActiveDrawing(assemblyDrawing);
+			}
+
+			private static string GetDrawingUsableMark(Drawing drawing)
+			{
+				var mark = string.Empty;
+				if (drawing is AssemblyDrawing)
+				{
+					var part =
+						new Model().SelectModelObject(((AssemblyDrawing)drawing).AssemblyIdentifier);
+					part?.GetReportProperty("ASSEMBLY_POS", ref mark);
+					return mark;
+				}
+				if (drawing is SinglePartDrawing)
+				{
+					var part =
+						new Model().SelectModelObject(((SinglePartDrawing)drawing).PartIdentifier);
+					part?.GetReportProperty("ASSEMBLY_POS", ref mark);
+					return mark;
+				}
+				return string.Empty;
+			}
+		}
+	}
+}
--- OpenShopDrawing/Program.cs
+++ OpenShopDrawing/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace OpenShopDrawing
 {
-	class Program
+	public partial class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			try
+			{
+				OpenShopDrawing.Run();
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine(ex.InnerException + ex.Message + ex.StackTrace);
+			}
 		}
 	}
 }
